c9bfc200b6052b8f590cd3a228a21a43df05e64d
surajwebo <surajmirajkar@rediffmail.com>
2015-10-01 11:06:19 +0530

    Initial Commit

---
+++ Contacts-iOS9-ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contacts-iOS9-ObjC
+//
+//  Created by Suraj on 01/10/15.
+//  Copyright © 2015 Suraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Contacts-iOS9-ObjC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Contacts-iOS9-ObjC
+//
+//  Created by Suraj on 01/10/15.
+//  Copyright © 2015 Suraj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
